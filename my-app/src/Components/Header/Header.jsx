747d5c52e751ae5c95ee5eb86de2c0c02a9315c9
V0LT0R <119123327+V0LT0R@users.noreply.github.com>
2024-04-23 12:41:48 +0500

    commit

--- my-app/src/Components/Header/Header.jsx
+++ my-app/src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './Header.module.css';
 import {Link} from "react-router-dom";
-import {EnvironmentOutlined} from "@ant-design/icons";
+import {EnvironmentOutlined, GiftOutlined, HeartOutlined, UserOutlined} from "@ant-design/icons";
 
 const Header = () => {
     return (
@@ -10,11 +10,22 @@ const Header = () => {
                 <div className={classes.wrapper}>
                     <div className={classes.location}>
                         <Link to={'/'} className={classes.link_base}>
-                            <h3>Location <EnvironmentOutlined /></h3>
+                            <h3 className={classes.h3_header}>Location <EnvironmentOutlined className={classes.icon_head}/></h3>
                         </Link>
                     </div>
 
-                    <div></div>
+                    <div className={classes.othertxt}>
+                        <Link to={'/'} className={classes.link_base}>
+                            <h3 className={classes.h3_header}><HeartOutlined className={classes.icon_head}/> Give Socks, Get Socks</h3>
+                        </Link>
+                        <Link to={'/'} className={classes.link_base}>
+                            <h3 className={classes.h3_header}><GiftOutlined className={classes.icon_head}/> Gift Cards</h3>
+                        </Link>
+                        <Link to={'/'} className={classes.link_base}>
+                            <h3 className={classes.h3_header}><UserOutlined className={classes.icon_head}/> Log In</h3>
+                        </Link>
+
+                    </div>
                 </div>
             </div>
             <div className={classes.wrapper}>
